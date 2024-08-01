f27726fc149bd76e8f3ad8280501677ff50af971
amirfarzamnia <amir2452fa@gmail.com>
2024-08-01 23:09:28 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,17 @@ import { Grid, Box, Card, CardContent, Typography, Container, Divider } from '@m
 const cards = [
     { title: 'Card 1', description: 'This is the first card.' },
     { title: 'Card 2', description: 'This is the second card.' },
-    { title: 'Card 3', description: 'مشخصات محصول پس از بررسی توسط کارشناسان سروبان یا نماینده محلی، برای خریداران احتمالی ارسال و مذاکرات بازاریابی توسط تیم سروبان انجام می شود. تلاش سروبان بر ایجاد زبان مشترک میان فروشنده و خریدار و ایجاد یک معامله منصفانه خواهد بود' }
+    { title: 'تحویل', description: 'مشخصات محصول پس از بررسی توسط کارشناسان سروبان یا نماینده محلی، برای خریداران احتمالی ارسال و مذاکرات بازاریابی توسط تیم سروبان انجام می شود. تلاش سروبان بر ایجاد زبان مشترک میان فروشنده و خریدار و ایجاد یک معامله منصفانه خواهد بود' }
 ];
 
 export default () => {
     return (
         <Container className="mt-10" maxWidth="xl">
-            <Typography textAlign="center" variant="h4">
+            <Typography className="text-teal-400" gutterBottom textAlign="center" variant="h4">
                 تناج چگونه کار میکند؟
+            </Typography>
+            <Typography textAlign="center" variant="subtitle2">
+                مستقیم از قلب مردم
             </Typography>
             <Box sx={{ my: 4, width: '25%', mx: 'auto' }}>
                 <Divider />
@@ -20,7 +23,7 @@ export default () => {
             <Grid container spacing={2}>
                 {cards.map((card) => (
                     <Grid item xs={12} sm={6} md={4}>
-                        <Card>
+                        <Card sx={{ background: 'transparent' }}>
                             <CardContent>
                                 <Typography variant="h5" component="div">
                                     {card.title}
