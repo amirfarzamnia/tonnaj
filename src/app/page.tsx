36bef76c060b65337494b9ecd8d33a3e4ffb0f20
amirfarzamnia <amir2452fa@gmail.com>
2024-08-02 02:01:38 +0330

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Grid, Box, Button, Card, Paper, ImageList, ImageListItem, ImageListItemBar, ListSubheader, IconButton, CardContent, Typography, Container, Divider } from '@mui/material';
+import { Grid, Box, Button, Card, Paper, ImageList, ImageListItem, ImageListItemBar, CardContent, Typography, Container, Divider, CardActionArea, CardActions, CardMedia } from '@mui/material';
 import { Inventory2, Sell, LocalShipping, Pageview } from '@mui/icons-material';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
@@ -32,49 +32,93 @@ const cards = [
     { title: 'تحویل', icon: LocalShipping, description: 'مشخصات محصول پس از بررسی توسط کارشناسان سروبان یا نماینده محلی، برای خریداران احتمالی ارسال و مذاکرات بازاریابی توسط تیم سروبان انجام می شود. تلاش سروبان بر ایجاد زبان مشترک میان فروشنده و خریدار و ایجاد یک معامله منصفانه خواهد بود' }
 ];
 
-export default () => {
-    const sliderSettings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-
-    const itemData = [
+const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
         {
-            img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-            title: 'Breakfast',
-            author: '@bkristastucchio'
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true
+            }
         },
         {
-            img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-            title: 'Burger',
-            author: '@rollelflex_graphy726'
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-    ];
+    ]
+};
 
+const itemData = [
+    {
+        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        title: 'Breakfast',
+        author: '@bkristastucchio'
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        title: 'Burger',
+        author: '@rollelflex_graphy726'
+    }
+];
+
+const products = [
+    {
+        title: 'غذای چینی',
+        description: 'غذای چینی بسیار خوشمزه',
+        img: 'https://material-ui.com/static/images/cards/contemplative-reptile.jpg',
+        author: {
+            name: 'سارا',
+            city: 'تهران'
+        }
+    }
+];
+
+export default () => {
     return (
         <Container className="mt-10" maxWidth="xl">
+            <Grid container spacing={2} sx={{ px: 2 }}>
+                {products.map((card, index) => (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                                <CardMedia component="img" height="140" image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg" alt="green iguana" />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div" className="flex items-center justify-between">
+                                        {card.title}
+                                        <Box component="small" className="text-gray-400 text-sm">
+                                            {card.author.city} &#x2022; {card.author.name}
+                                        </Box>
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {card.description}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button sx={{ width: '100%', py: 1 }} variant="outlined" color="success">
+                                    تماس با فروشنده
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+            <Box className="mx-auto my-24 w-full flex items-center justify-center">
+                <Button variant="outlined" color="secondary" href="/market-guid">
+                    تمام محصولات بازار را ببینید
+                </Button>
+            </Box>
             <ImageList sx={{ width: '85%', height: 450, mx: 'auto', mb: 8 }}>
                 {itemData.map((item) => (
                     <div key={item.img} className="relative group">
