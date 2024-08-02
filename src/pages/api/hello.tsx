fac3cbe1109e931129591ef0437d41f66d39635e
amirfarzamnia <amir2452fa@gmail.com>
2024-08-02 04:39:18 +0330

    Added env

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,9 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { database } from '@/mongodb';
 
-export default (req: NextApiRequest, res: NextApiResponse): void => {
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     if (req.method === 'GET') {
         res.status(200).json({ message: 'Hello, World!' });
+
+        console.log(await database.collections());
     } else {
         res.setHeader('Allow', ['GET']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
