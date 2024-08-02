181fc5a2d8fd9045f353476a055aad752d4060ec
amirfarzamnia <amir2452fa@gmail.com>
2024-08-02 17:28:10 +0330

    s

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -5,14 +5,14 @@ import { database } from '@/mongodb';
 export const POST = async (request: NextRequest) => {
     const { phone_number, verification_code } = await request.json();
 
-    return NextResponse.json({ error: 'Test' }, { status: 400 });
-
     if (phone_number) {
-        KavenegarApi({ apikey: '6B57554970696A54724C6536785034716559324A6B78734D734C304E4C4C623073494A4E3574782B4C70303D' }).Send({ message: 'خدمات پیام کوتاه کاوه نگار', sender: '1000689696', receptor: '09130288776' }, () => {
+        KavenegarApi({ apikey: '6B57554970696A54724C6536785034716559324A6B78734D734C304E4C4C623073494A4E3574782B4C70303D' }).Send({ message: 'خدمات پیام کوتاه کاوه نگار', sender: '1000689696', receptor: phone_number }, () => {
             console.log(true);
         });
     } else if (verification_code) {
     } else {
         return NextResponse.json({ error: 'شماره تماس و یا کد تایید وارد نشده!' });
     }
+
+    return NextResponse.json({ error: 'Test' }, { status: 400 });
 };
