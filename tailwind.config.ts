0761d9208f7ae97197741cf83e11abf81892573f
amirfarzamnia <amir2452fa@gmail.com>
2024-08-02 04:33:40 +0330

    improvements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,7 @@ import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      boxShadow: {
-        bg: '2px 10px 50px'
-      }
-    }
-  },
-  darkMode: 'class',
-  plugins: [nextui({})]
+  plugins: [nextui()]
 };
 
 export default config;
