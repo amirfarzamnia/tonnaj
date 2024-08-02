65d70529a9b06b6f1269e68dc9ac9667e38c40ff
amirfarzamnia <amir2452fa@gmail.com>
2024-08-02 14:14:06 +0330

    added logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -134,26 +134,9 @@ export default ({ children }: { children: React.ReactNode }) => {
                 <body>
                     <AppBar position="static">
                         <Toolbar className={`border-b ${darkMode ? 'border-zinc-800' : 'border-zinc-200'} px-3`} disableGutters>
-                            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                            <Typography variant="h6" noWrap component="a" href="#app-bar-with-responsive-menu" sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', textDecoration: 'none' }}>
-                                LOGO
-                            </Typography>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                                <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu}>
-                                    <MenuIcon />
-                                </IconButton>
-                                <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left' }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' } }}>
-                                    {pages.map((page) => (
-                                        <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                            <Typography textAlign="center">{page}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </Menu>
-                            </Box>
-                            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                            <Typography variant="h5" noWrap component="a" href="#app-bar-with-responsive-menu" sx={{ mr: 2, display: { xs: 'flex', md: 'none' }, flexGrow: 1, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', textDecoration: 'none' }}>
-                                LOGO
-                            </Typography>
+                            <Link href="/" underline="none">
+                                <Box width={150} component="img" alt="لوگوی تناژ" src="icons/tonnaj.png"></Box>
+                            </Link>
                             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                                 {pages.map((page) => (
                                     <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, display: 'block' }}>
@@ -161,25 +144,23 @@ export default ({ children }: { children: React.ReactNode }) => {
                                     </Button>
                                 ))}
                             </Box>
-                            <Box sx={{ flexGrow: 0 }}>
-                                <Tooltip title="Toggle theme">
-                                    <IconButton onClick={toggleTheme} sx={{ p: 0 }}>
-                                        {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
-                                    </IconButton>
-                                </Tooltip>
-                                <Tooltip title="Open settings">
-                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                        <Avatar alt="ا" src="/static/images/avatar/2.jpg" />
-                                    </IconButton>
-                                </Tooltip>
-                                <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={Boolean(anchorElUser)} onClose={handleCloseUserMenu}>
-                                    {settings.map((setting) => (
-                                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                            <Typography textAlign="center">{setting}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </Menu>
-                            </Box>
+                            <Tooltip title="Toggle theme">
+                                <IconButton onClick={toggleTheme} sx={{ p: 0 }}>
+                                    {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar alt="ا" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={Boolean(anchorElUser)} onClose={handleCloseUserMenu}>
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center">{setting}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
                         </Toolbar>
                         <Toolbar className={`border-b ${darkMode ? 'border-zinc-800' : 'border-zinc-200'}`}>
                             <TextField
