f6481dd41d054ef2b1691127dc17b8e77ba99947
amirfarzamnia <amir2452fa@gmail.com>
2024-08-05 01:25:19 +0330

    i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default () => {
             <Grid container spacing={3}>
                 {(selectedCategories.length > 0 ? products.filter((product) => product.categories.some((category) => selectedCategories.includes(category))) : products).map(({ price, description, images, title, id }) => (
                     <Grid item xs={12} sm={6} md={2} key={id}>
-                        <Link href={`/products/${id}`} passHref>
+                        <Link href={`/products/${id}`} passHref style={{ textDecoration: 'none' }}>
                             <Card>
                                 <Swiper spaceBetween={10} slidesPerView={1} pagination={{ clickable: true }} style={{ width: '100%', height: 'auto' }}>
                                     {images.map((image, index) => (
