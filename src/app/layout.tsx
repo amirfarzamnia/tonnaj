9661b1b1edfeaac9a5cc68d6f621254bcb693c0b
amirfarzamnia <amir2452fa@gmail.com>
2024-08-05 04:15:12 +0330

    Merge branch 'main' of https://github.com/amirfarzamnia/tonnaj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { Button, CircularProgress, TextField, Box, Grid, Toolbar, AppBar, Link, Avatar, Tooltip, Container, Typography, InputAdornment, IconButton, CssBaseline, Shadows, MenuItem, Stack, List, ListItem, ListItemText } from '@mui/material';
-import { Search as SearchIcon, LightMode as LightModeIcon, DarkMode as DarkModeIcon, Menu } from '@mui/icons-material';
+import { Button, CircularProgress, TextField, Box, Grid, Toolbar, AppBar, Link, Avatar, Tooltip, Container, Typography, InputAdornment, IconButton, CssBaseline, Shadows } from '@mui/material';
+import { Search as SearchIcon, LightMode as LightModeIcon, DarkMode as DarkModeIcon } from '@mui/icons-material';
 import { createTheme, ThemeProvider, ThemeOptions } from '@mui/material/styles';
 import { TypographyOptions } from '@mui/material/styles/createTypography';
 import AuthProvider from '@/contexts/auth';
 import ShopProvider from '@/contexts/shop';
 import BlogProvider from '@/contexts/blog';
-import React, { useState } from 'react';
+import React from 'react';
 import './index.css';
 
 const common: { typography: TypographyOptions; css: React.CSSProperties } = {
@@ -94,8 +94,6 @@ const schemeOptions: { dark: ThemeOptions; light: ThemeOptions } = {
 export default ({ children }: { children: React.ReactNode }) => {
     const [selectedTheme, setTheme] = React.useState<'dark' | 'light'>('light');
     const [loading, setLoading] = React.useState(true);
-    const [isOpen, setIsOpen] = useState<boolean>(false)
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     React.useEffect(() => {
         const theme = (localStorage.getItem('selected-theme') as 'dark' | 'light') || (matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
@@ -117,61 +115,6 @@ export default ({ children }: { children: React.ReactNode }) => {
                         </Box>
                     ) : (
                         <>
-                            <Box sx={{
-                                position: "fixed",
-                                top: isOpen ? 0 : '-100vh', // Slide down when isOpen is true, slide up when false
-                                width: '100%',
-                                height: "100vh",
-                                transition: 'top 0.5s ease', // Transition for sliding effect
-                                backgroundColor: "black",
-                                zIndex: 50,
-                                display: "flex",
-                                flexDirection: 'column',
-                                alignItems: "center",
-                                justifyContent: 'start'
-                            }}>
-                                <Box sx={{ width: "100%", display: 'flex', justifyContent: 'end', alignItems: 'center', padding: '1px', borderBottom: 2 }} >
-                                    <MenuItem sx={{ ml: 1, color: 'white' }} onClick={() => setIsOpen(false)}>بستن</MenuItem>
-                                </Box>
-
-                                <Box sx={{ width: "100%", display: 'flex', justifyContent: 'end', alignItems: 'center', padding: '1px', position: 'relative', mt: 2 }} >
-                                    <MenuItem
-                                        sx={{ ml: 1, color: 'white' }}
-                                        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                                    >
-                                        فروشگاه
-                                    </MenuItem>
-                                    {isDropdownOpen && (
-                                        <Box
-                                            sx={{
-                                                position: 'absolute',
-                                                top: '100%',
-                                                right: 0,
-                                                backgroundColor: 'black',
-                                                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                                                zIndex: 100,
-                                                width: '100%',
-                                                display: 'flex',
-                                                justifyContent: "center",
-                                                alignItems: 'center',
-                                            }}
-                                        >
-                                            <List sx={{ width: '100%' }}>
-                                                <ListItem button>
-                                                    <ListItemText primary="Item 1" sx={{ color: 'white', ml: 2 }} />
-                                                </ListItem>
-                                                <ListItem button>
-                                                    <ListItemText primary="Item 2" sx={{ color: 'white', ml: 2 }} />
-                                                </ListItem>
-                                                <ListItem button>
-                                                    <ListItemText primary="Item 3" sx={{ color: 'white', ml: 2 }} />
-                                                </ListItem>
-                                            </List>
-                                        </Box>
-                                    )}
-                                </Box>
-                            </Box>
-
                             <AppBar position="static">
                                 <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', borderBottom: 1, borderColor: selectedTheme === 'dark' ? '#3f3f46' : '#e4e4e7', px: 3 }}>
                                     <Link href="/" underline="none">
@@ -214,8 +157,12 @@ export default ({ children }: { children: React.ReactNode }) => {
                                     <Button variant="outlined">Outlined</Button>
                                 </Toolbar>
                                 <Toolbar sx={{ borderBottom: 1, borderColor: selectedTheme === 'dark' ? '#3f3f46' : '#e4e4e7', justifyContent: 'center' }}>
-                                    <Box sx={{ width: "100%", height: "5vh", display: "flex", justifyContent: 'end', alignItems: 'center' }} maxWidth={'100%'}>
-                                        <Menu sx={{ padding: '2px', borderRadius: "10px", transition: "all .1s ease", fontSize: "30px", border: "2px solid transparent", ":active": { border: "2px solid white" } }} onClick={() => setIsOpen(true)} />
+                                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                                        {['محصولات', 'قیمتها', 'تعرفه خدمات', 'تماس با تناژ', 'خدمات تناژ', 'داستان تناژ', 'بازار عمده تناژ'].map((page) => (
+                                            <Button key={page} sx={{ my: 2, display: 'block' }}>
+                                                {page}
+                                            </Button>
+                                        ))}
                                     </Box>
                                 </Toolbar>
                             </AppBar>
