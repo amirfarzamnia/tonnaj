32a0dd26bb548bc7b2bb20c3a1ad5b406cdc55cf
amirfarzamnia <amir2452fa@gmail.com>
2024-08-05 11:31:48 +0330

    update

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,1 +1,9 @@
-export type AuthTypes = { phone_number: string } | { phone_number: string; verification_code: string };
+interface PhoneNumber {
+    phone_number: string;
+}
+
+interface VerificationCode extends PhoneNumber {
+    verification_code: string;
+}
+
+export type AuthTypes = PhoneNumber | VerificationCode;
