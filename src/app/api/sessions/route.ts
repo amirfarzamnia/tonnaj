6dc714967eede1abe89528038ce1d4865c0972dc
amirfarzamnia <amir2452fa@gmail.com>
2024-08-05 23:39:32 +0330

    update

--- src/app/api/sessions/route.ts
+++ src/app/api/sessions/route.ts
@@ -2,9 +2,21 @@ import { NextRequest, NextResponse } from 'next/server';
 import { database } from '@/mongodb';
 
 export const GET = async (request: NextRequest) => {
-    const session = await database.collection('sessions').findOne({ session: request.cookies.get('session')?.value });
+    const session = await findSession(request);
 
     if (!session) return new NextResponse(null, { status: 400 });
 
     return NextResponse.json(session, { status: 200 });
 };
+
+export const DELETE = async (request: NextRequest) => {
+    const session = await findSession(request);
+
+    if (!session) return new NextResponse(null, { status: 403 });
+
+    return NextResponse.json(session, { status: 200 });
+};
+
+async function findSession(request: NextRequest) {
+    return await database.collection('sessions').findOne({ session: request.cookies.get('session')?.value });
+}
