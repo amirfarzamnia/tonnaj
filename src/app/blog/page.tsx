a5111336f07dd4c90d7e6fdca177a3560c26f4f4
amirfarzamnia <amir2452fa@gmail.com>
2024-08-07 23:15:57 +0330

    update

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,28 +1,9 @@
 'use client';
 
-import { Button, Box, Card, CardActions, CardContent, Divider, Grid, Stack, styled, Typography, CircularProgress, Alert } from '@mui/material';
+import { Button, Box, Card, CardActions, CardContent, Divider, Grid, Stack, Typography, CircularProgress, Alert } from '@mui/material';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { BlogTypes } from '@/types/blog';
-
-const StyledCard = styled(Card)(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    height: '100%',
-    boxShadow: theme.shadows[3],
-    borderRadius: theme.shape.borderRadius * 2,
-    transition: 'transform 0.3s, box-shadow 0.3s'
-}));
-
-const StyledButton = styled(Button)(({ theme }) => ({
-    width: '100%',
-    border: '2.5px solid transparent',
-    borderRadius: theme.shape.borderRadius * 2,
-    textTransform: 'uppercase',
-    fontWeight: theme.typography.fontWeightBold,
-    transition: 'all 0.2s ease-in'
-}));
 
 export default () => {
     const [error, setError] = useState<string | null>(null);
@@ -69,26 +50,32 @@ export default () => {
     return (
         <Stack maxWidth={'100%'}>
             <Grid container spacing={3} justifyContent="center">
-                {blogs.map((blog, index) => (
-                    <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
-                        <StyledCard>
-                            <Box component="img" src={blog.image} loading="lazy" height={'30vh'} sx={{ border: '1px solid white', borderRadius: '10px' }} />
-                            <CardContent>
-                                <Typography variant="h6" sx={{ textAlign: 'center', mb: 1 }}>
-                                    {blog.name}
-                                </Typography>
-                                <Divider sx={{ mb: 2 }} />
-                                <Typography variant="body2" color="text.secondary" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 4, WebkitBoxOrient: 'vertical', height: 95 }}>
-                                    {blog.content}
-                                </Typography>
-                            </CardContent>
+                {blogs.map(({ content, name, image, categories }, index) => (
+                    <Grid item xs={12} sm={6} md={3} key={index}>
+                        <Card sx={{ borderRadius: 4 }}>
+                            <Box component="img" src={image} loading="lazy" height={'30vh'} />
+                            <Typography variant="h6" sx={{ textAlign: 'center', mb: 1 }}>
+                                {name}
+                            </Typography>
+                            <Divider sx={{ mb: 2 }} />
+                            <Typography variant="body2" color="text.secondary" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 4, WebkitBoxOrient: 'vertical', height: 95 }}>
+                                {content}
+                            </Typography>
                             <Divider />
-                            <CardActions sx={{ p: 2, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                <StyledButton href={`/blog/${blog.name}`} variant="outlined" color="primary">
-                                    ادامه
-                                </StyledButton>
-                            </CardActions>
-                        </StyledCard>
+                            <Button href={'/blog/' + name} variant="outlined" color="success" sx={{ width: '100%' }}>
+                                مشاهده
+                            </Button>
+                            <Box sx={{ my: 2 }}>
+                                <Divider />
+                            </Box>
+                            <Typography variant="body2" color="textSecondary">
+                                {categories.map((category, index) => (
+                                    <Box component="small" key={index}>
+                                        {category}
+                                    </Box>
+                                ))}
+                            </Typography>
+                        </Card>
                     </Grid>
                 ))}
             </Grid>
