cc0787a8e4bb1f1a545c970d2d0b2c810997f356
amirfarzamnia <amir2452fa@gmail.com>
2024-08-08 22:12:26 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,15 +136,15 @@ export default () => {
                     {productRequests.map(({ id, author, description, location }) => (
                         <SwiperSlide key={id} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '90%', height: 'auto' }}>
                             <Card sx={{ width: 345, height: '100%', borderRadius: 4, display: 'flex', flexDirection: 'column' }}>
-                                <CardContent sx={{ flex: 1 }}>
-                                    <Typography variant="h6" gutterBottom textAlign="center">
+                                <CardContent sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+                                    <Typography variant="h6" gutterBottom textAlign="center" fontSize="medium">
                                         {author.name} از {location.city}
                                     </Typography>
                                     <Box sx={{ my: 2 }}>
                                         <Divider />
                                     </Box>
-                                    <Typography variant="body1" color="textSecondary" gutterBottom>
-                                        {description.slice(0, 165)}...
+                                    <Typography variant="body1" color="textSecondary" gutterBottom sx={{ flex: 1, overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 3 }}>
+                                        {description}
                                     </Typography>
                                     <Button endIcon={<Phone />} href={'tel:' + author.phone_number} variant="outlined" color="success" sx={{ mt: 2, width: '100%', py: 2, display: 'flex', alignItems: 'center', gap: 2, borderRadius: 1 }} onClick={(event) => event.stopPropagation()}>
                                         تماس با خریدار
