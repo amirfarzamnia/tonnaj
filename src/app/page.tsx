0692d33c9e1aaab0939aeda7b88424580488a3a3
amirfarzamnia <amir2452fa@gmail.com>
2024-08-08 22:13:50 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -146,7 +146,7 @@ export default () => {
                                     <Typography variant="body1" color="textSecondary" gutterBottom sx={{ flex: 1, overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 3 }}>
                                         {description}
                                     </Typography>
-                                    <Button endIcon={<Phone />} href={'tel:' + author.phone_number} variant="outlined" color="success" sx={{ mt: 2, width: '100%', py: 2, display: 'flex', alignItems: 'center', gap: 2, borderRadius: 1 }} onClick={(event) => event.stopPropagation()}>
+                                    <Button endIcon={<Phone />} href={'tel:' + author.phone_number} variant="outlined" color="secondary" sx={{ mt: 2, width: '100%', py: 2, display: 'flex', alignItems: 'center', gap: 2, borderRadius: 1 }} onClick={(event) => event.stopPropagation()}>
                                         تماس با خریدار
                                     </Button>
                                     <Box sx={{ my: 2 }}>
