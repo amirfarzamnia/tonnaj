97192174b9a9b5e214de188e5d8e995c114f781e
amirfarzamnia <amir2452fa@gmail.com>
2024-08-10 20:17:38 +0330

    update

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,29 +1,27 @@
 import { Typography, Box, Divider, Card } from '@mui/material';
 
-export default () => {
-    return (
-        <Card elevation={3} sx={{ padding: 4, mx: 50, my: 10, borderRadius: 4 }}>
-            <Typography variant="h4" component="h1" gutterBottom textAlign="center">
-                تماس با ما
+export default () => (
+    <Card elevation={3} sx={{ padding: 4, mx: 50, my: 10, borderRadius: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom textAlign="center">
+            تماس با ما
+        </Typography>
+        <Box mt={2}>
+            <Typography variant="body1" paragraph textAlign="center">
+                برای تماس با تیم تناژ میتوانید از اطلاعات زیر استفاده کنید.
             </Typography>
-            <Box mt={2}>
-                <Typography variant="body1" paragraph textAlign="center">
-                    برای تماس با تیم تناژ میتوانید از اطلاعات زیر استفاده کنید.
-                </Typography>
-                <Divider sx={{ my: 4 }} />
-                <Typography variant="h6" component="h2" gutterBottom sx={{ mb: 4 }}>
-                    اطلاعات تماس
-                </Typography>
-                <Typography variant="body1" paragraph>
-                    ایمیل: info@tonnaj.com
-                </Typography>
-                <Typography variant="body1" paragraph>
-                    شماره تماس: 02122924090
-                </Typography>
-                <Typography variant="body1" paragraph>
-                    آدرس: تهران، اقدسیه، خیابان ارتش، پلاک ۱۱۳
-                </Typography>
-            </Box>
-        </Card>
-    );
-};
+            <Divider sx={{ my: 4 }} />
+            <Typography variant="h6" component="h2" gutterBottom sx={{ mb: 4 }}>
+                اطلاعات تماس
+            </Typography>
+            <Typography variant="body1" paragraph>
+                ایمیل: info@tonnaj.com
+            </Typography>
+            <Typography variant="body1" paragraph>
+                شماره تماس: 02122924090
+            </Typography>
+            <Typography variant="body1" paragraph>
+                آدرس: تهران، اقدسیه، خیابان ارتش، پلاک ۱۱۳
+            </Typography>
+        </Box>
+    </Card>
+);
