917e1a9e53a4b6aaa9bd382eef41aa4c023901ed
amirfarzamnia <amir2452fa@gmail.com>
2024-08-10 10:57:43 +0330

    update

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -55,23 +55,14 @@ export const GET = async (request: NextRequest) => {
         name: { name: 1 }
     };
 
-    const filterKeys = searchParams.get('filters')?.split(',') || [];
-    const sort = filterKeys.map((cat) => cat.trim()).reduce((acc, key) => (sortOptions[key] ? { ...acc, ...sortOptions[key] } : acc), {});
-
-    const filter = id ? { id } : categories ? { categories: { $in: categories.split(',').map((cat) => cat.trim()) } } : {};
-
     const start = parseInt(searchParams.get('start') || '0', 10);
     const end = parseInt(searchParams.get('end') || '10', 10);
 
-    const collection = searchParams.get('type') === 'request' ? 'product_requests' : 'products';
-
-    const data = await database
-        .collection(collection)
-        .find(filter)
-        .sort(sort)
-        .skip(start)
-        .limit(end - start)
-        .toArray();
+    const collectionRef = database.collection(searchParams.get('type') === 'request' ? 'product_requests' : 'products');
+    const query = collectionRef.find(id ? { id } : categories ? { categories: { $in: categories.split(',').map((cat) => cat.trim()) } } : {});
+    const sortedQuery = query.sort((searchParams.get('filters')?.split(',') || []).map((cat) => cat.trim()).reduce((acc, key) => (sortOptions[key] ? { ...acc, ...sortOptions[key] } : acc), {}));
+    const paginatedQuery = sortedQuery.skip(start).limit(end - start);
+    const data = await paginatedQuery.toArray();
 
     return NextResponse.json(data, { status: 200 });
 };
