401686dea3d7e7f0fdac78f2b69e0e90410a8c95
amirfarzamnia <amir2452fa@gmail.com>
2024-08-10 11:22:17 +0330

    update

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,6 +2,7 @@
 
 import { Grid, Typography, Box, CircularProgress, Button } from '@mui/material';
 import ProductCard from '@/components/ProductCard';
+import { Inventory } from '@mui/icons-material';
 import { ProductTypes } from '@/types/product';
 import React from 'react';
 
@@ -66,6 +67,19 @@ export default () => {
 
     return (
         <>
+            <Box sx={{ boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.3)', my: 4, px: 10, height: '230px', borderRadius: 4, background: '#feb204', display: 'flex', justifyContent: 'space-between', gap: 2 }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, height: '100%', justifyContent: 'center' }}>
+                    <Typography variant="h6" color="black" fontWeight="bold">
+                        شما در این بخش میتوانید محصولات موجود در تناژ را مشاهده کنید.
+                    </Typography>
+                    <Box>
+                        <Button size="large" endIcon={<Inventory />} href="/products/create" variant="contained" color="success" sx={{ display: 'flex', alignItems: 'center', gap: 1.25, py: 0.82, width: 'fit-content' }}>
+                            شماهم محصول خود را ثبت کنید
+                        </Button>
+                    </Box>
+                </Box>
+                <Box component="img" src="/images/pages/products/banner.png" sx={{ objectFit: 'cover', height: '230px' }} />
+            </Box>
             <Grid container spacing={3}>
                 {products.map((product) => (
                     <Grid item xs={12} sm={6} md={3} key={product.id}>
