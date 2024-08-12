10c386d1b249a8705b8f6408dacc9c21e10e74c6
amirfarzamnia <amir2452fa@gmail.com>
2024-08-12 13:29:52 +0330

    update

--- src/layouts/product.tsx
+++ src/layouts/product.tsx
@@ -66,7 +66,7 @@ export default ({ method }: { method: 'create' | 'request' }) => {
 
     return (
         <>
-            <Box sx={{ height: '300px', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.3)', border: 1, borderColor: 'grey.600', backgroundImage: 'url("/images/pages/products/' + method + '/banner.png")', backgroundSize: 'cover', backgroundPosition: 'center', position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center', borderRadius: 4, mb: 10 }}></Box>
+            <Box sx={{ height: { xs: '180px', lg: '400' }, boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.3)', border: 1, borderColor: 'grey.600', backgroundImage: 'url("/images/pages/products/' + method + '/banner.png")', backgroundSize: 'cover', backgroundPosition: 'center', position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center', borderRadius: 4, mb: 10 }}></Box>
             <Box
                 component="form"
                 onSubmit={async (e: React.FormEvent) => {
