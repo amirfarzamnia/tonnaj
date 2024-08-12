319933860c17bd51f84318086d29db754e4b4de5
amirfarzamnia <amir2452fa@gmail.com>
2024-08-12 19:49:32 +0330

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -143,10 +143,10 @@ export default ({ children }: { children: React.ReactNode }) => {
                             <AppBar position="fixed">
                                 <Toolbar sx={{ display: 'flex', alignItems: 'center', gap: 2, justifyContent: 'space-between', borderBottom: 1, borderColor: selectedTheme === 'dark' ? '#3f3f46' : '#e4e4e7', px: 3 }}>
                                     <Link href="/" underline="none">
-                                        <Box width="15vw" maxWidth="85px" component="img" loading="lazy" alt="لوگوی تناژ" src="/images/icons/tonnaj.png" />
+                                        <Box width="15vw" maxWidth="90px" component="img" loading="lazy" alt="لوگوی تناژ" src="/images/icons/tonnaj.png" />
                                     </Link>
-                                    <Button startIcon={<MenuIcon />} sx={{ display: 'flex', alignItems: 'center', gap: 1, py: 0.82 }} onClick={(event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget)}>
-                                        <Box sx={{ display: { xs: 'none', md: 'block' } }}>دسته بندی ها</Box>
+                                    <Button variant="outlined" startIcon={<MenuIcon />} sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', gap: 1, py: 0.82 }} onClick={(event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget)}>
+                                        دسته بندی ها
                                     </Button>
                                     <TextField
                                         placeholder="جست و جوی محصول..."
